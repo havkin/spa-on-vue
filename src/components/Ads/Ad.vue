0ae00b93254046ed4f81ed045e7f39cade1450d9
Maxim Sozinov <max.sozinov@gmail.com>
2020-02-20 14:56:01 +0700

    5

---
+++ src/components/Ads/Ad.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+     <v-row>
+        <v-col>
+           <h1>Ad</h1>
+        </v-col>
+     </v-row>
+  </v-container>  
+</template>
+
+<script>
+export default {
+  data: () => ({
+    
+  })
+}
+</script>
+
+<style>
+
+</style>
