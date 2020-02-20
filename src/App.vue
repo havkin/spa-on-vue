bea5e209152bf5ed157bcc9c600a87d56ef906c1
Maxim Sozinov <max.sozinov@gmail.com>
2020-02-20 13:57:42 +0700

    4

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,36 @@
 <template>
   <v-app>
-    <v-navigation-drawer app>
-      <!-- -->
+    <v-navigation-drawer app v-model="drawer" temporary>
+      <v-list>
+        <v-list-item v-for="item in links" :key="item.title" :to="item.url">
+          <v-list-item-icon>
+            <v-icon v-if="item.icon">mdi-{{item.icon}}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title"></v-list-item-title>
+          </v-list-item-content>
+
+          <v-list-item-avatar>
+            <v-img :src="item.avatar"></v-img>
+          </v-list-item-avatar>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
     <v-app-bar app dense color="primary" dark>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="hidden-md-and-up"></v-app-bar-nav-icon> 
 
       <v-toolbar-title>Ad application</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
-      <v-btn text height="100%" tile>Normal</v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn text height="100%" tile v-for="item in links" :key="item.title" :to="item.url">
+          <v-icon left>mdi-{{item.icon}}</v-icon>
+          {{item.title}}
+        </v-btn>
+      </v-toolbar-items>
     </v-app-bar>
 
     <v-content>
@@ -32,6 +41,16 @@
 
 <script>
 export default {
-  name: "App"
+  name: "App",
+  data: () => ({
+    drawer: false,
+    links: [
+      { title: "Login", icon: "lock", url: "/login" },
+      { title: "Registration", icon: "face", url: "/registration" },
+      { title: "Orders", icon: "bookmark-outline", url: "/orders" },
+      { title: "New ad", icon: "file-plus", url: "/new" },
+      { title: "My ads", icon: "format-list-bulleted", url: "/list" }
+    ]
+  })
 };
 </script>
